72b7ac98d8014bc633351b92fa6fdbb211cc1425
Rixy Husky <120264529+R1XY@users.noreply.github.com>
2023-10-08 22:18:14 -0400

    Initial Push

---
+++ README.md
@@ -0,0 +1,58 @@
+
+# Linux Maintenance Wizard 🧙‍♂️
+
+## Overview
+
+The Linux Maintenance Wizard is a colorful and interactive Bash script designed to streamline your Linux package management. The script includes options for updating packages, upgrading packages, and cleaning up, all presented in an easy-to-use interactive menu. With colorful text and emojis, this script makes maintaining your Linux system a breeze.
+
+## Features
+
+- 🌈 Colorful and Interactive UI
+- ℹ️ Informative prompts and completion statuses
+- 🆙 Option for updating packages
+- ⏫ Option for upgrading packages
+- 🧹 Option for cleaning up
+- 📦 Option to perform all the above operations in one go
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/YourUsername/Linux-Maintenance-Wizard.git
+    ```
+
+2. Navigate to the script's directory:
+
+    ```bash
+    cd Linux-Maintenance-Wizard
+    ```
+
+3. Make the script executable:
+
+    ```bash
+    chmod +x linux_maintenance_wizard.sh
+    ```
+
+## Usage
+
+To run the script, simply execute:
+
+```bash
+./linux_maintenance_wizard.sh
+```
+
+Follow the on-screen prompts to complete the tasks.
+
+## Requirements
+
+- Bash
+- sudo access
+
+## Contributing
+
+Feel free to fork the repository and submit pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ update.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+# Initialize Colors
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+END="\e[0m"
+
+# Initialize Emojis
+SUCCESS="👍"
+FAIL="👎"
+INFO="ℹ️"
+QUESTION="❓"
+
+# Function to print header
+print_header() {
+  echo -e "${CYAN}########################################${END}"
+  echo -e "${CYAN}###### Linux Maintenance Wizard #########${END}"
+  echo -e "${CYAN}########################################${END}"
+}
+
+# Function to execute a command and print a success or failure message
+execute_and_print() {
+  local description=$1
+  local command=$2
+
+  echo -e "${YELLOW}${INFO} ${description}${END}"
+  eval $command
+
+  if [ $? -eq 0 ]; then
+    echo -e "${GREEN}${SUCCESS} Completed successfully!${END}"
+  else
+    echo -e "${RED}${FAIL} Failed to complete!${END}"
+  fi
+}
+
+# Function to handle package updates
+handle_updates() {
+  execute_and_print "Updating Packages" "sudo apt update"
+}
+
+# Function to handle package upgrades
+handle_upgrades() {
+  execute_and_print "Upgrading Packages" "sudo apt upgrade -y"
+  execute_and_print "Performing Distribution Upgrades" "sudo apt dist-upgrade -y"
+}
+
+# Function to handle cleanup
+handle_cleanup() {
+  execute_and_print "Cleaning Up" "sudo apt clean"
+  execute_and_print "Autocleaning" "sudo apt autoclean"
+  execute_and_print "Autoremoving" "sudo apt autoremove"
+}
+
+# Function to show wizard menu
+show_menu() {
+  echo -e "${MAGENTA}${QUESTION} What would you like to do?${END}"
+  echo -e "${BLUE}1) Update Packages${END}"
+  echo -e "${BLUE}2) Upgrade Packages${END}"
+  echo -e "${BLUE}3) Clean Up${END}"
+  echo -e "${BLUE}4) Everything (Update, Upgrade, and Clean Up)${END}"
+  echo -e "${BLUE}5) Exit${END}"
+
+  read -p "Enter your choice [1-5]: " choice
+
+  case $choice in
+    1) handle_updates ;;
+    2) handle_upgrades ;;
+    3) handle_cleanup ;;
+    4) handle_updates; handle_upgrades; handle_cleanup ;;
+    5) echo -e "${GREEN}Exiting the wizard. Goodbye!${END}"; exit 0 ;;
+    *) echo -e "${RED}Invalid choice, exiting.${END}"; exit 1 ;;
+  esac
+}
+
+# Main Program
+clear
+print_header
+show_menu
